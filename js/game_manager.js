c873c15ea38b1a2c4dfe30f6a790327be02ae94c
ForgettableUsername <49412506+ForgettableUsername@users.noreply.github.com>
2019-04-08 15:07:51 -0700

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -37,7 +37,7 @@ GameManager.prototype.isGameTerminated = function () {
 GameManager.prototype.setup = function () {
   this.grid        = new Grid(this.size);
 
-  this.score       = 696969696969696969696969696969696969696969696969696969696969696969;
+  this.score       = "weener";
   this.over        = false;
   this.won         = false;
   this.keepPlaying = false;
